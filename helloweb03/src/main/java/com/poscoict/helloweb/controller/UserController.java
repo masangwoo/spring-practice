66e1a5645f4a8f8911a0efc757675c21258ed4e4
user <user@DESKTOP-EKQUFAN>
2022-01-21 11:11:48 +0900

    spring practice #4

--- helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
@@ -3,6 +3,8 @@ package com.poscoict.helloweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * @RequestMapping : 클래스(타입) + 메서드(핸들러) 매핑
@@ -21,6 +23,26 @@ public class UserController {
 	public String join(UserVo vo) {
 		System.out.println(vo);
 		return "redirect:/";
+	}
+	
+	/*
+	 * 만일, n이라는 이름의 파라미터가 없는 경우
+	 * 400 bad request에러가 발생한다.
+	 */
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		System.out.println(name);
+		return "UserController.update()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=true, defaultValue="guest") String name, 
+						  @RequestParam(value="a", required=true, defaultValue="0") Integer age) {
+		System.out.println(name);
+		System.out.println(age);
+		return "UserController.update()";
 	}
 }
 
