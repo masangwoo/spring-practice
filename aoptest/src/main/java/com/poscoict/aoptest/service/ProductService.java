d6efb46ce7832b48c7baf502c2eb77e218fed0e1
user <user@DESKTOP-EKQUFAN>
2022-01-27 12:33:02 +0900

    AOP practice

---
+++ aoptest/src/main/java/com/poscoict/aoptest/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.poscoict.aoptest.service;
+
+import org.springframework.stereotype.Service;
+
+import com.poscoict.aoptest.vo.ProductVo;
+
+@Service
+public class ProductService {
+	public ProductVo find(String name) {
+		System.out.println("[ProductService] finding...");
+		
+		/*if(true) {
+			throw new RuntimeException("Find Exception");
+			}*/
+		
+		
+		return new ProductVo(name);
+	}
+
+}
